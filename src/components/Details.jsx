215c10c76f933beada03f6f0d54e67de9cdd4749
Somesh <imsomeshkd@gmail.com>
2021-09-02 00:36:44 +0530

    covid-19 state tracker

---
+++ src/components/Details.jsx
@@ -0,0 +1,48 @@
+import React, {useEffect, useState} from 'react';
+
+const Details = () => {
+    const [data, setData] = useState([]);
+    const getCovidData = async () => {
+        try {
+            const res = await fetch('https://data.covid19india.org/data.json');
+            const newVar = await res.json();
+            setData(newVar.statewise[26])
+        } catch (e) {
+            console.log(e)
+        }
+
+    }
+
+    useEffect(() => {
+        getCovidData();
+    }, [])
+
+    return (
+        <div>
+            <h1>Covid-19 Tracker Application</h1>
+
+            <table id="customers">
+                <tr>
+                    <th>Active</th>
+                    <th>Confirmed</th>
+                    <th>Deaths</th>
+                    <th>Recovered</th>
+                    <th>State</th>
+                    <th>State code</th>
+                    {/*<th>State Notes</th>*/}
+                </tr>
+                <tr>
+                    <td>{data.active}</td>
+                    <td>{data.confirmed}</td>
+                    <td>{data.deaths}</td>
+                    <td>{data.recovered}</td>
+                    <td>{data.state}</td>
+                    <td>{data.statecode}</td>
+                    {/*<td>{data.statenotes}</td>*/}
+                </tr>
+            </table>
+        </div>
+    );
+};
+
+export default Details;
